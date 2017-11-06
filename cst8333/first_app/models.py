a06c9b6a7ed60cb7cb61f06c50fd9f0ccdc0c4e6
Philip <hohohsh@icloud.com>
2017-11-06 11:59:27 -0500

    Adding Bootstrap and font-awesome folder

--- cst8333/first_app/models.py
+++ cst8333/first_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class dataset(models.Model):
+    ref_date = models.DecimalField(max_digits=4, decimal_places=0)
+    geo = models.CharField(max_length=40)
+    est = models.CharField(max_length=150)
+    vector = models.CharField(max_length=6)
+    coordinate = models.CharField(max_length=4)
+    value = models.DecimalField(max_digits=10, decimal_places=0)
